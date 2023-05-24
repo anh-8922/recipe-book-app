17fe146a4e3d5bdb957b25cc7d11cb308bb480ce
Anh Chau <anhchau8922@gmail.com>
2023-05-24 18:47:22 +0100

    make layout, routing system, homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import kitchen from './Assets/kitchen.jpg';
+import './css/styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import '@coreui/coreui/dist/css/coreui.min.css'
+import Home from "./Pages/home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="main" style={{ backgroundImage: `url(${kitchen})`, backgroundSize: "cover", height: '100vh'}}>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
